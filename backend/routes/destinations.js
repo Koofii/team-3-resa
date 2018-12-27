a043edbdc304b31976aa57aae4dd65163f7b466a
Koofii <kristoffer.frejd@gmail.com>
2018-12-27 12:51:35 +0100

    backend-structure ok

---
+++ backend/routes/destinations.js
@@ -0,0 +1,9 @@
+const route = require('express').Router();
+const destinations = require('../controllers/destinationController');
+
+route.get('/getalldestinations', destinations.getAllDestinations);
+route.post('/getdestinationtype', destinations.getDestinationsPerType);
+route.post('/getdestinationdate', destinations.getDestinationsPerDate);
+
+
+module.exports = route;
