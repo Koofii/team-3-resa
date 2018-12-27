ce932208a0b5cbd261e7099bc759058bf47e2794
Koofii <kristoffer.frejd@gmail.com>
2018-12-27 15:43:03 +0100

    fixded header and footer

--- resa-team-3/src/App.js
+++ resa-team-3/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Footer from './components/footer';
+import Header from './components/header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-         
-       
+          <Header/>
+          
+          <Footer />
         </header>
       </div>
     );
