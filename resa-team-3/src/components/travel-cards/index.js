95cdacc08351f0c1b4b7aa73c40f4053442928c0
Linnea Agetorp <Linnea Agetorp@LAPTOP-AHL0EBJT>
2018-12-28 13:38:07 +0100

    catch errors on fetching destinations

--- resa-team-3/src/components/travel-cards/index.js
+++ resa-team-3/src/components/travel-cards/index.js
@@ -5,32 +5,46 @@ import Card from './card';
 export class TravelCards extends Component {
     state = {
         destinations: [],
-        currentDestinations: [] //on component-update, update sorted destinations here
+        currentDestinations: [], //on component-update, update sorted destinations here
+        error: null
     };
-    
-componentDidMount(){
-    fetch('http://localhost:3001/getalldestinations')
-    .then(res => res.json())
-    .then(data => this.setState({
-        destinations: data.whereTo,       // keep the original list here, unchanged
-        currentDestinations: data.whereTo // when the component first loads, put all destinations here, for initial rendering
-    }))
-};
 
-  render() {
-      if(this.state.destinations.length > 0) {
-        return (
-            <div className="cards-container">
-              
-              {this.state.currentDestinations.map((destination, i) => {
-                return <Card key={i} data={{destination, i}}/>
-              })}
-            </div>
-          );
-      } else {
-          return (
-              <div>Loading...</div>
-          )
-      }
-  }
+    componentDidMount() {
+        fetch('http://localhost:3001/getalldestinations')
+            .then(res => {
+                if (res.status === 200) {
+                    return res.json();
+                }
+                throw new Error(`Something went wrong, received status: ${res.status}, message: ${res.statusText}`);
+            })
+            .then(data => {
+                this.setState({
+                    destinations: data.whereTo,       // keep the original list here, unchanged
+                    currentDestinations: data.whereTo // when the component first loads, put all destinations here, for initial rendering
+                })
+            })
+            .catch(error => {
+
+                this.setState({
+                    error: error.message
+                })
+            })
+    };
+
+    render() {
+        if (this.state.destinations.length > 0) {
+            return (
+                <div className="cards-container">
+
+                    {this.state.currentDestinations.map((destination, i) => {
+                        return <Card key={i} data={{ destination, i }} />
+                    })}
+                </div>
+            );
+        } else {
+            return (
+                <div className="information"> {this.state.error ? this.state.error : "Loading..."}</div>
+            )
+        }
+    }
 }
