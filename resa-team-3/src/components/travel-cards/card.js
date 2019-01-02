0c37f8f7f301489655b746915cb8dfb598a23258
Linnea Agetorp <Linnea Agetorp@LAPTOP-AHL0EBJT>
2019-01-02 11:31:27 +0100

    add book-alert

--- resa-team-3/src/components/travel-cards/card.js
+++ resa-team-3/src/components/travel-cards/card.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import './card.css';
 
-export default ({data}) => {
+export default class Card extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    handleBooking() {
+        alert(`Resa till ${this.props.data.destination.city} bokat! Trevlig resa`)
+    }
     
-    return (
-        <div key={data.i} className='card-body'>
-        
-            <h4 className="card-title">{data.destination.city}, {data.destination.country}</h4>
-            <img className="card-img" src={data.destination.image} alt={data.destination.city}  />
-            <div className="btn-container">
-                <div className="book-btn-box" onClick={()=> alert("Bokat! Trevlig resa")}>
-                    <a href="#" className="book-btn">Boka</a>
-                </div>
-                <div className="read-more-btn-box">
-                    <a href="#" className="read-more-btn">Läs mer</a>
+    render() {
+        return (
+            <div key={this.props.data.i} className='card-body'>
+            
+                <h4 className="card-title">{this.props.data.destination.city}, {this.props.data.destination.country}</h4>
+                <img className="card-img" src={this.props.data.destination.image} alt={this.props.data.destination.city}  />
+                <div className="btn-container">
+                    <div className="book-btn-box" onClick={this.handleBooking.bind(this)}>
+                        <a href="#" className="book-btn">Boka</a>
+                    </div>
+                    <div className="read-more-btn-box">
+                        <a href="#" className="read-more-btn">Läs mer</a>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
